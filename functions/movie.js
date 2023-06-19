dbe386f2ab01119ca0e8f6ddecb1ed0ded2104ec
Will Adams <bushblade@gmail.com>
2023-06-19 16:41:07 +0100

    remove console log

--- functions/movie.js
+++ functions/movie.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 
 exports.handler = async (event) => {
   const endpoint = event.path.replace('/api', '')
-  console.log(endpoint)
 
   const response = await fetch(
     `${process.env.TMDB_API_URL}${endpoint}?api_key=${process.env.TMDB_API_KEY}&language=en-US`
